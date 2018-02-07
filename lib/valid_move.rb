dfde24fff1b85b53b1d5733c12beaf1be1f87340
Igli Cekani <htnacekan@gmail.com>
2018-02-07 17:26:47 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -15,10 +15,9 @@ def position_taken? (board, index)
 end
 
 def valid_move?(board, index)
- if position_taken?==false
+ if position_taken?
    return false
- else
-   return true
+ else return true
  end
 
 
